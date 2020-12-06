2b08edcec16b60434e8529e578a390805d68d6d7
Aleksei Kibardin <soadiv@gmail.com>
2020-12-07 01:35:33 +0400

    fix all security warnings, del video link where necessary

--- src/Display.js
+++ src/Display.js
@@ -3,19 +3,22 @@ function Display(props) {
     return (
         <div class="App-result-content__content">
         <h1>Моделирование ННР: {props.name}</h1>
-        <iframe src={props.url}
-        frameBorder='0'
-        allow='autoplay; encrypted-media'
-        allowFullScreen
-        width='80%'
-        height='900'
-        className='App-result-content__video'
-        title='video'/>
+        {(props.url) &&
+            <iframe src={props.url}
+            frameBorder='0'
+            allow='autoplay; encrypted-media'
+            allowFullScreen
+            width='80%'
+            height='900'
+            className='App-result-content__video'
+            title='video'/>
+        }
         {(props.day) &&
             <div className="alink">
             <a
                 href={props.day}
                 target="_blank"
+                rel="noreferrer"
             >
                 Суточный план-график
             </a> 
@@ -26,12 +29,13 @@ function Display(props) {
             <a
                 href={props.graphik}
                 target="_blank"
+                rel="noreferrer"
             >
                 График исполненного движения
             </a> 
             </div>
         }
-        {ar[0] != "" &&
+        {ar[0] !== "" &&
         <div>
             <ul>{ar.map((item, index) => (
             <li className="ListItem">
